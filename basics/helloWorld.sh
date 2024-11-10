b9787694fea95aa40a753f18b2af23f4c6de4f62
Hunter Watkins <doctorofplagues35@gmail.com>
2024-11-10 10:58:38 +0000

    added some bash scripts

---
+++ basics/helloWorld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+myName="Hunter"
+myAge="30"
+myUsername=$(whoami)
+date=$(date)
+
+echo "Hello World, my name is $myName."
+echo "My age is $myAge."
+echo "My username is $myUsername."
+echo "Today's date is $date."
